3cba22a85832429ccdadbe8bb64fcac352712c55
davidnboffelli <vboxuser@Ubuntuinterfaz.myguest.virtualbox.org>
2024-04-24 10:26:58 +0200

    Incorporo los hosts al inventario. Continuo con el ejercicio5

--- soluciones/dnazareno-ejer5/comandos.sh
+++ soluciones/dnazareno-ejer5/comandos.sh
@@ -3,4 +3,25 @@
 ansible-playbook -i /home/vboxuser/Workspace/gitrepo/ansible-exercises-by-groups-3-2-ansible/soluciones/dnazareno-ejer1/inventario playbook-ejer1.yaml
 
 #2
+#playbook-ejer2.yaml
 ansible-playbook -i /home/vboxuser/Workspace/gitrepo/ansible-exercises-by-groups-3-2-ansible/soluciones/dnazareno-ejer1/inventario playbook-ejer2.yaml
+
+#3
+#playbook-ejer3.yaml
+ansible-playbook -i /home/vboxuser/Workspace/gitrepo/ansible-exercises-by-groups-3-2-ansible/soluciones/dnazareno-ejer1/inventario playbook-ejer3.yaml
+
+#4
+#playbook-ejer4.yaml
+ansible-playbook -i /home/vboxuser/Workspace/gitrepo/ansible-exercises-by-groups-3-2-ansible/soluciones/dnazareno-ejer1/inventario playbook-ejer4.yaml
+
+#5
+#playbook-ejer5.yaml
+ansible-playbook -i /home/vboxuser/Workspace/gitrepo/ansible-exercises-by-groups-3-2-ansible/soluciones/dnazareno-ejer1/inventario playbook-ejer5.yaml
+
+#6
+#playbook-ejer6.yaml
+ansible-playbook -i /home/vboxuser/Workspace/gitrepo/ansible-exercises-by-groups-3-2-ansible/soluciones/dnazareno-ejer1/inventario playbook-ejer6.yaml
+
+#7
+#playbook-ejer7.yaml
+ansible-playbook -i /home/vboxuser/Workspace/gitrepo/ansible-exercises-by-groups-3-2-ansible/soluciones/dnazareno-ejer1/inventario playbook-ejer7.yaml
